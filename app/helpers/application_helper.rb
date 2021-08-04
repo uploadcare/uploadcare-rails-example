9df42426877efa5cf5cefddbb883fcd73ca2884d
dmitrijivanchenko <dmitrij.ivanchenko@gmail.com>
2021-08-04 13:19:59 +0600

    ID-3: App config (#1)
    * Add gems and config
    
    * Add CI
    
    * Add github actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  FLASH = {
+    'notice' => 'alert alert-info',
+    'success' => 'alert alert-success',
+    'error' => 'alert alert-error',
+    'alert' => 'alert alert-warning'
+  }.freeze
+
+  def flash_class(level)
+    FLASH.with_indifferent_access[level]
+  end
 end
