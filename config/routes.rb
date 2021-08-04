7b2d06e0592452fee555870613bdb06f55e1d8da
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-08-04 16:54:22 +0600

    Fix file store, script

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Files
   resources :files, only: %i[index show destroy]
-  get '/store_file/:id', to: 'files#store', as: 'store_file'
+  post '/store_file/:id', to: 'files#store', as: 'store_file'
   get '/new_store_file_batch', to: 'files#new_store_file_batch', as: 'new_store_file_batch'
   get '/new_delete_file_batch', to: 'files#new_delete_file_batch', as: 'new_delete_file_batch'
   post '/store_file_batch', to: 'files#store_file_batch', as: 'store_file_batch'
