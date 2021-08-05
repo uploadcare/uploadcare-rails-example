3f17bf9281c05706aa0b272808a94756361f1620
dmitrijivanchenko <dmitrij.ivanchenko@gmail.com>
2021-08-05 16:18:07 +0600

    ID-3: Video conversions (#5)
    * Add document conversions
    
    * Add video conversions. Fix document conversions

---
+++ app/controllers/conversions/base_conversions_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Conversions
+  class BaseConversionsController < ApplicationController
+    private
+
+    def parse_problem
+      parsed_problem = JSON.parse(params[:problem].tr(':', '').gsub('=>', ':')).to_a.flatten
+      form_problem_result(parsed_problem[0], parsed_problem[1])
+    rescue JSON::ParserError, TypeError, NoMethodError
+      form_problem_result(nil, params[:problem])
+    end
+
+    def form_problem_result(source, reason)
+      { original_source: source, reason: reason }
+    end
+
+    def throw_error?
+      params[:throw_error].present?
+    end
+
+    def obtain_remote_files
+      @files_data = Uploadcare::FileApi.get_files
+      @files = @files_data[:results]
+    end
+  end
+end
