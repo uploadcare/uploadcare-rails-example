8ea979e282ae28c9a618f48efdc9d91e9a322981
Vipul A M <vipulnsward@gmail.com>
2024-03-10 02:43:16 +0530

    Pin hotwired libraries and fix imports for turbo. Also fix importmap tags in application layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import "@hotwired/turbo-rails"
-import "app/javascript/controllers"
-import "@rails/ujs"
+import Rails from "@rails/ujs"
+import {Turbo} from "@hotwired/turbo-rails"
 
-
+Turbo.start()
 Rails.start()
