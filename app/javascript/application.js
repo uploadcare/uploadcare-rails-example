998864b90cb79da7f77bb0cae8b47f1d8e613b3b
Vipul A M <vipulnsward@gmail.com>
2024-03-30 18:11:02 +0530

    Add workaround for know issue related to turbo/turbolinks widget caching

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,23 @@ import {Turbo} from "@hotwired/turbo-rails"
 
 Turbo.start()
 Rails.start()
+
+
+// Know issue in widget, see https://github.com/uploadcare/uploadcare-rails/issues/134
+
+document.addEventListener('turbo:before-cache', function() {
+    const dialogClose = document.querySelector('.uploadcare--dialog__close');
+    if (dialogClose) {
+        dialogClose.dispatchEvent(new Event('click'));
+    }
+
+    const dialog = document.querySelector('.uploadcare--dialog');
+    if (dialog) {
+        dialog.remove();
+    }
+
+    const widgets = document.querySelectorAll('.uploadcare--widget');
+    widgets.forEach(widget => {
+        widget.remove();
+    });
+});
