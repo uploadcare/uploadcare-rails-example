55e93ac0cac4ebc5e69c25b186f0c2a78c1d6db1
Vipul A M <vipulnsward@gmail.com>
2026-02-21 20:33:02 +0530

    Use gem-provided ActiveStorage integration and drop local patches

--- spec/initializers/active_storage_uploadcare_spec.rb
+++ spec/initializers/active_storage_uploadcare_spec.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'ActiveStorage Uploadcare initializer' do
-  it 'registers uploadcare previewer in configured previewers' do
+RSpec.describe "Uploadcare::Rails ActiveStorage integration" do
+  it "registers uploadcare previewer in configured previewers" do
     previewers = Rails.application.config.active_storage.previewers
-
-    expect(previewers).to include(ActiveStorage::Previewer::UploadcarePreviewer)
+    expect(previewers).to include(Uploadcare::Rails::ActiveStorage::UploadcarePreviewer)
   end
 
-  it 'prepends uploadcare variant processing to ActiveStorage::Variant' do
-    expect(ActiveStorage::Variant.ancestors).to include(ActiveStorage::VariantUploadcareRemoteProcessing)
+  it "prepends uploadcare variant processing to ActiveStorage::Variant" do
+    expect(ActiveStorage::Variant.ancestors).to include(Uploadcare::Rails::ActiveStorage::VariantRemoteProcessing)
   end
 end
